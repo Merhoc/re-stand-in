b926ae30b2cdde36977c1128779fb31851612ae1
heikometzger@heikometzger.de <heikometzger@heikometzger.de@f10eb394-0640-26f7-dd84-8af7ae832db2>
2012-05-18 21:00:55 +0000

    Copyright, cleaning up.

--- trunk/moc_rsi.cpp
+++ trunk/moc_rsi.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'rsi.h'
 **
-** Created: Fri May 18 21:42:48 2012
+** Created: Fri May 18 23:00:00 2012
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.1)
 **
 ** WARNING! All changes made in this file will be lost!
--- trunk/ui_rsi.h
+++ trunk/ui_rsi.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'rsi.ui'
 **
-** Created: Mon 7. May 21:06:24 2012
+** Created: Fri May 18 22:51:20 2012
 **      by: Qt User Interface Compiler version 4.8.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
